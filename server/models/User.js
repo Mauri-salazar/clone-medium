43a966d092d4aa43c6cfb81cfcc2d3b10c390eeb
Mauri-salazar <salazar.mauricio.dev@gmail.com>
2023-08-16 15:52:39 -0300

    creation funtion method AddFollower in model User

--- server/models/User.js
+++ server/models/User.js
@@ -23,8 +23,16 @@ let UserSchema = new mongoose.Schema(
     }
 );
 
+
+// If the user is not in the follower list, let them follow you
 UserSchema.methods.follow = function (user_id) {
     if (this.following.indexof(user_id) === -1) {
         this.following.push(user_id);
     }
 };
+
+UserSchema.methods.AddFollower = function (fs) {
+    this.followers.push(fs);
+};
+
+module.exports = mongoose.model('User', UserSchema);
