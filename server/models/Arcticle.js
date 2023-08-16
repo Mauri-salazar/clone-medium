9b682eff0fec3796689e1d73b6cd4858801a6aa2
Mauri-salazar <salazar.mauricio.dev@gmail.com>
2023-08-16 15:45:53 -0300

    creating funtion method getUserArticle in model Acticle.js and creation the model User [schema and funtion  methods follow ]

--- server/models/Arcticle.js
+++ server/models/Arcticle.js
@@ -24,6 +24,24 @@ let ArticleSchema = new mongoose.Schema(
 );
 
 ArticleSchema.methods.clap = function () {
-    this.claps++
+    this.claps++;
     return this.save();
 };
+
+ArticleSchema.methods.comment = function (c) {
+    this.comments.push(c);
+    return this.save();
+};
+
+ArticleSchema.methods.addAuthor = function (author_id) {
+    this.author = author_id;
+    return this.save(); 
+};
+
+ArticleSchema.methods.getUserArticle = function (_id) {
+    Article.find({'author': _id}).then( (article) => {
+        return article;
+    })
+};
+
+module.exports = mongoose.model('Article', ArticleSchema);
